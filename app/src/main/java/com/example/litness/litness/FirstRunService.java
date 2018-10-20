28189bfcc4c69e06feb895b1ea0c8cd74cc3a9f3
Jake Mizzell <jakemizzell@gmail.com>
2018-10-20 12:05:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/litness/litness/FirstRunService.java
@@ -0,0 +1,13 @@
+package com.example.litness.litness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstRunService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_run_service);
+    }
+}
